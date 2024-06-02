e7ba30e5363e8979731fb570cc7d4a12c5e374a1
Cleiton Braga <cleitonbraga56@gmail.com>
2024-06-02 00:59:35 -0300

    fix: Fix 'User' model import

--- modules/users/src/lib/users/shared/user/user.service.ts
+++ modules/users/src/lib/users/shared/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { User } from '../modules/User';
+import { User } from '../../modules/User';
 
 @Injectable({
   providedIn: 'root'
