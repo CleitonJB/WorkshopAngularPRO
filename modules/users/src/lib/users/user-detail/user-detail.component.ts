289e043894c9bcb5832c85f19f165a95289d0c8f
Cleiton Braga <cleitonbraga56@gmail.com>
2024-06-02 00:42:25 -0300

    feat: Get user data by route id param

---
+++ modules/users/src/lib/users/user-detail/user-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { UserService } from '../shared/user.service';
+import { User } from '../modules/User';
+
+@Component({
+  selector: 'app-user-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-detail.component.html',
+  styleUrl: './user-detail.component.scss',
+})
+export class UserDetailComponent implements OnInit {
+  constructor(
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+      this.activatedRoute.params.subscribe({
+        next: (data: Params) => {
+          console.log(data['id']);
+          this.userService.getUserById(data['id']).subscribe({
+            next: (userData: User) => {
+              console.log(userData);
+            },
+            error: (error: unknown) => {
+              console.error(error);
+            }
+          });
+        },
+        error: (error: unknown) => {
+          console.error(error);
+        }
+      });
+  }
+}
