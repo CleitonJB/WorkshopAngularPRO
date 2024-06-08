409e69053e8dfae27a5c141dca35e9b71e5c593d
Cleiton Braga <cleitonbraga56@gmail.com>
2024-06-08 00:33:03 -0300

    test: Fix and Add 'users' component tests

--- modules/users/src/lib/users/users.component.spec.ts
+++ modules/users/src/lib/users/users.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UsersComponent } from './users.component';
 
+import { of } from 'rxjs';
+import { provideRouter } from '@angular/router';
+
+import { mockUsers } from './mocks/user.mock';
+
+import { UserService } from './shared/user/user.service';
+
 describe('UsersComponent', () => {
   let component: UsersComponent;
   let fixture: ComponentFixture<UsersComponent>;
@@ -8,6 +15,15 @@ describe('UsersComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [UsersComponent],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            getUsers: () => of(mockUsers),
+          }
+        },
+        provideRouter([])
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UsersComponent);
@@ -17,5 +33,15 @@ describe('UsersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should list users', () => {
+    const users: HTMLElement[] = fixture.nativeElement.querySelectorAll('li');
+
+    expect(users.length).toBe(mockUsers.length);
+
+    users.forEach((user, index) => {
+      expect(user.textContent).toBe(mockUsers[index].name);
+    });
   });
 });
